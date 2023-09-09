5b46bb8071c758163283598eb11a49ed7a0c77b2
Dmitry Kolesnikov <zalimannard@mail.ru>
2023-09-09 03:13:09 +0300

    feat: Implement GET/tracks/{trackId} endpoint

--- internal/handlers/track/read.go
+++ internal/handlers/track/read.go
@@ -3,8 +3,49 @@ package track
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog/log"
+	"music-files/internal/handlers/types"
+	"net/http"
+	"strconv"
+	"time"
 )
 
+type readResponse struct {
+	TrackId   int       `json:"trackId"`
+	CoverId   int       `json:"coverId,omitempty"`
+	Extension string    `json:"extension"`
+	Size      int64     `json:"size"`
+	DateAdded time.Time `json:"dateAdded"`
+}
+
 func (h *Handler) Read(c *gin.Context) {
-	log.Panic().Msg("Not implemented")
+	log.Debug().Msg("Fetching data about track")
+
+	trackIdStr := c.Param("trackId")
+	trackId, err := strconv.Atoi(trackIdStr)
+	if err != nil {
+		log.Error().Err(err).Msg("Invalid trackId format")
+		c.JSON(http.StatusBadRequest, types.Error{
+			Error: "Invalid trackId format",
+		})
+		return
+	}
+	log.Debug().Int("trackId", trackId).Msg("Url parameter read successfully")
+
+	track, err := h.TrackRepo.Read(trackId)
+	if err != nil {
+		log.Error().Err(err).Msg("Failed to read track")
+		c.JSON(http.StatusInternalServerError, types.Error{
+			Error: "Failed to read track",
+		})
+		return
+	}
+
+	log.Debug().Int("trackId", track.TrackId).Str("relativePath", track.RelativePath).Msg("Track fetched successfully")
+	c.JSON(http.StatusOK, readResponse{
+		TrackId:   track.TrackId,
+		CoverId:   *track.CoverId,
+		Extension: track.Extension,
+		Size:      track.Size,
+		DateAdded: track.DateAdded,
+	})
 }
