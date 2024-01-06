83cb5efe44f010c7de2532793902f6fbc65459e3
Dmitry Kolesnikov <zalimannard@mail.ru>
2024-01-06 22:22:15 +0500

    feat: Preparation of endpoints and handlers

--- internal/app/http.go
+++ internal/app/http.go
@@ -6,7 +6,38 @@ import (
 )
 
 func (a *App) RegisterRoutes() {
-	_ = a.router.Group("/api")
+	api := a.router.Group("/api")
+	{
+		roots := api.Group("/roots")
+		{
+			roots.POST("", a.handler.AddRoot)
+			roots.GET("", a.handler.GetRoots)
+			roots.DELETE("/:dirId", a.handler.DeleteRoot)
+		}
+
+		dirs := api.Group("/dirs")
+		{
+			dirs.GET("/:dirId", a.handler.GetDir)
+			dirs.GET("/:dirId/content", a.handler.GetDirContent)
+			dirs.POST("/:dirId/scan", a.handler.ScanDir)
+			dirs.POST("/scan", a.handler.ScanDirs)
+		}
+
+		audios := api.Group("/audios")
+		{
+			audios.GET("", a.handler.GetAudios)
+			audios.GET("/all", a.handler.GetAllAudios)
+			audios.GET("/:audioId/download", a.handler.DownloadAudio)
+			audios.GET("/best-covers", a.handler.GetBestCovers)
+			audios.GET("/sha256/:sha256", a.handler.SearchBySha256)
+		}
+
+		covers := api.Group("/covers")
+		{
+			covers.GET("", a.handler.GetCovers)
+			covers.GET("/:coverId/image", a.handler.GetStaticImage)
+		}
+	}
 }
 
 func (a *App) StartHTTPServer(cfg config.HTTPConfig) error {
---
+++ internal/handler/search_by_sha_256.go
@@ -0,0 +1,7 @@
+package handler
+
+import "github.com/gin-gonic/gin"
+
+func (h Handler) SearchBySha256(context *gin.Context) {
+
+}
