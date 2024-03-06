fa8d4a6e1fe1522c1a281f35949bcb26ee9fa86a
zalimannard <zalimannard@mail.ru>
2024-03-07 00:28:35 +0300

    feat: Reading yml config

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/rs/zerolog/log"
+	"music-files/internal/config"
+)
 
 func main() {
-	fmt.Println("Приложение запустилось")
+	cfg, err := config.Parse()
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to get config")
+	}
+	fmt.Println(cfg)
 }
---
+++ internal/config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"github.com/spf13/viper"
+	"music-files/pkg/util"
+)
+
+type Config struct {
+	Logger LoggerConfig `mapstructure:"logger"`
+	DB     DBConfig     `mapstructure:"database"`
+	HTTP   HTTPConfig   `mapstructure:"http"`
+}
+
+type LoggerConfig struct {
+	Level string `mapstructure:"level" default:"info"`
+}
+
+type DBConfig struct {
+	Host     string `mapstructure:"host" required:"yes"`
+	Port     int    `mapstructure:"port" required:"yes"`
+	Username string `mapstructure:"username" required:"yes"`
+	Password string `mapstructure:"password" required:"yes"`
+	Name     string `mapstructure:"name" required:"yes"`
+}
+
+type HTTPConfig struct {
+	Host string `mapstructure:"host" required:"yes"`
+	Port int    `mapstructure:"port" required:"yes"`
+}
+
+func Parse() (*Config, error) {
+	viper.AddConfigPath("./config")
+	viper.AddConfigPath(".")
+
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, util.WrapError(err, "failed to read config file")
+	}
+
+	var config Config
+	err = viper.Unmarshal(&config)
+	if err != nil {
+		return nil, util.WrapError(err, "failed to unmarshal config")
+	}
+
+	return &config, nil
+}
