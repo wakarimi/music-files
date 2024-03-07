137f903d5f4301fde251bcc141bce15c2114256c
zalimannard <zalimannard@mail.ru>
2024-03-07 22:51:08 +0300

    ref: Move initialization to app.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"log"
-	"music-files/internal/config"
-	pkglogger "music-files/pkg/logger"
-)
+import "music-files/internal/app"
 
 func main() {
-	cfg, err := config.Parse()
-	if err != nil {
-		log.Fatalf("Failed to get config. %v", err)
-	}
-
-	logger, err := pkglogger.NewZerologLogger(&cfg.Logger)
-	if err != nil {
-		log.Fatalf("Failed to create logger. %v", err)
-	}
-	logger.Info("Logger initialized")
+	app.Run()
 }
---
+++ internal/app/app.go
@@ -0,0 +1,20 @@
+package app
+
+import (
+	"log"
+	"music-files/internal/config"
+	pkglogger "music-files/pkg/logger"
+)
+
+func Run() {
+	cfg, err := config.Parse()
+	if err != nil {
+		log.Fatalf("Failed to get config. %v", err)
+	}
+
+	logger, err := pkglogger.NewZerologLogger(&cfg.Logger)
+	if err != nil {
+		log.Fatalf("Failed to create logger. %v", err)
+	}
+	logger.Info("Logger initialized")
+}
